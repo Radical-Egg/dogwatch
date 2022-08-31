a2947f0f607b29f85389340369d8678120292f44
egg <luther.jaymen@gmail.com>
2022-08-31 13:42:07 -0700

    fixed threads updated readme

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # dogwatch
 
-You've been working the dogwatch for a few weeks now. It's past the point in the night where Fred Durst's career doesn't seem like a joke anymore, perhaps he was serious this whole time. You just need a power nap to get you through the rest of the night. Your manager has heen working remote for the past two years but he could show back up any day now. If only there was a way to keep my computer from sleeping with a single command.
+You've been working the dogwatch for a few weeks now. It's past the point in the night where Fred Durst's career doesn't seem like a joke anymore, perhaps he was serious this whole time. You just need a power nap to get you through the rest of the night. Your manager has heen working remote for the past two years but he could show back up any day now. If only there was a way to keep my computer from sleeping with a single command so that you can take a quick nap.
 
-
-### Usage
+## Usage
 
 - Stay awake forever by running ```dogwatch```
 - Stay awake for a specific amount of time (minutes) ```dogwatch --time 1```
 
+## Disclaimer
+
+Fred Durst might be a cool guy I don't actually know anything about him or his career. This is my frist project in Rust. Rust seems pretty cool. I wanted to learn about Dbus and this seemed like a cool way to do that. I found some good resources for anyone who is looking into Dbus.
+
+- [dbus-sample](https://github.com/makercrew/dbus-sample): This link is awesome and has a really good high level overview of dbus as well as a ton of useful links. There is also a sample cpp program on how to make a connection and introspect.
+- [dbus-rs](https://github.com/diwic/dbus-rs): D-Bus bindings for rust
+- [dbus tutorial](https://dbus.freedesktop.org/doc/dbus-tutorial.html): As you can tell by the CSS used on this webpage they would like to keep it strictly business
+- [Qdbusviewer](https://doc.qt.io/qt-6/qdbusviewer.html): I didn't find out about this until after I was done but this is a great way to mess around with Dbus before you start programming something. Here is the flathub link to download this jawn [flathub link](https://flathub.org/apps/details/io.qt.qdbusviewer)
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,8 @@
-use dbus::blocking::SyncConnection;
-use dbus::blocking::Proxy;
+use dbus::blocking::{SyncConnection, Proxy};
 use clap::Parser;
 use signal_hook::{consts::SIGINT, iterator::Signals};
-use std::{  process, time::Duration };
+use std::{ process, time::Duration, thread::sleep };
 use crossbeam_utils::thread;
-use std::thread::sleep;
-
-fn inhibit(proxy: &Proxy<&SyncConnection>) -> Result<u32, Box<dyn std::error::Error>> {
-    let (cookie,): (u32,) = proxy.method_call(
-        "org.freedesktop.ScreenSaver", 
-        "Inhibit", 
-        ("espresso", "requested by user Inhibiting Sleep via dogwatch"))?;
-    Ok(cookie)
-}
-
-fn uninhibit(proxy: &Proxy<&SyncConnection>, cookie: u32) -> Result<(), Box<dyn std::error::Error>> {
-    proxy.method_call(
-        "org.freedesktop.ScreenSaver", 
-        "UnInhibit", 
-        (cookie,))?;
-    Ok(println!("ScreenSaver enabled..Exiting!"))
-}
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -35,27 +17,46 @@ struct Args {
    time: u64,
 }
 
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn inhibit(proxy: &Proxy<&SyncConnection>) -> Result<u32, Box<dyn std::error::Error>> {
+    let (cookie,): (u32,) = proxy.method_call(
+        "org.freedesktop.ScreenSaver", 
+        "Inhibit", 
+        ("espresso", "requested by user Inhibiting Sleep via dogwatch"))?;
+    Ok(cookie)
+}
+
+fn uninhibit(proxy: &Proxy<&SyncConnection>, cookie: u32) -> Result<(), Box<dyn std::error::Error>> {
+    proxy.method_call(
+        "org.freedesktop.ScreenSaver", 
+        "UnInhibit", 
+        (cookie,))?;
+    Ok(println!("ScreenSaver and Sleep re-enabled...Exiting!"))
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let inhibit_cookie: u32;
     let args = Args::parse();
     let mut signals = Signals::new(&[SIGINT])?;
-
     let conn = SyncConnection::new_session()?;
     let proxy: Proxy<&SyncConnection> = conn.with_proxy("org.freedesktop.ScreenSaver", "/org/freedesktop/ScreenSaver", Duration::from_millis(5000));
+    let prox_copy = proxy.clone();
+
     inhibit_cookie = inhibit(&proxy)?;
+    println!("Sleep and ScreenSaver Disabled...Press Cntrl+C to Exit...");
 
     thread::scope(|s| {
         s.spawn(move |_| {
-            for sig in signals.forever() {
-                println!("Received signal {:?}", sig);
-                uninhibit(&proxy, inhibit_cookie).unwrap();
+            for _sig in signals.forever() {
+                uninhibit(&prox_copy, inhibit_cookie).unwrap();
                 process::exit(0x0100);
             }
         });
+        s.spawn(move |_| {
+            sleep(Duration::from_secs(60*args.time));
+            uninhibit(&proxy, inhibit_cookie).unwrap();
+            process::exit(0x0100);
+        });
     }).unwrap();
-
-
-    sleep(Duration::from_secs(60*args.time));
-    
+        
     Ok(())
 }
